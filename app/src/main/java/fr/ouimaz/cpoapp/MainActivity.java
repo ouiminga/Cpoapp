7bfd2fd1a433b14badf7fba5ef675f3622642393
ouiminga <ouimaz@yahoo.fr>
2023-04-08 23:10:07 +0200

    Login page appearance is ok
    logo +
    username +
    password +
    login button +
    signup option +

--- app/src/main/java/fr/ouimaz/cpoapp/MainActivity.java
+++ app/src/main/java/fr/ouimaz/cpoapp/MainActivity.java
@@ -1,23 +1,36 @@
 package fr.ouimaz.cpoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        ImageView logoImageView = findViewById(R.id.login_page_logo);
+        ConstraintLayout constraintlayout = findViewById(R.id.login_page_constraintlayout);
 
+        logoImageView.setOnClickListener(v -> Utils.hideKeyboard( context,  v));
+        constraintlayout.setOnClickListener(v -> Utils.hideKeyboard( context,  v));
     }
 
     public void homepage_login(View view) {
 
     }
+
     public void homepage_signup(View view) {
 
     }
+
 }
---
+++ app/src/main/java/fr/ouimaz/cpoapp/Utils.java
@@ -0,0 +1,18 @@
+package fr.ouimaz.cpoapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class Utils {
+
+    /**
+     Utility method to hide the soft keyboard.
+     @param context the context in which the method is being called.
+     @param view the view whose keyboard needs to be hidden.
+     */
+    public static void hideKeyboard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
