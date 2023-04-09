ec422571c8a71950189e69216e0642bf35054dda
ouiminga <ouimaz@yahoo.fr>
2023-04-09 02:50:16 +0200

    update

--- app/src/main/java/fr/ouimaz/cpoapp/MainActivity.java
+++ app/src/main/java/fr/ouimaz/cpoapp/MainActivity.java
@@ -1,17 +1,32 @@
 package fr.ouimaz.cpoapp;
 
+import static fr.ouimaz.cpoapp.Utils.hideKeyboard;
+import static fr.ouimaz.cpoapp.Utils.isValidPassword;
+import static fr.ouimaz.cpoapp.Utils.isValidUsername;
+import static fr.ouimaz.cpoapp.Utils.saveLoginState;
+import static fr.ouimaz.cpoapp.Utils.showInvalidUsernameMessage;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorBoundsInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import fr.ouimaz.cpoapp.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,27 +40,48 @@ public class MainActivity extends AppCompatActivity {
         ImageView logoImageView = findViewById(R.id.login_page_logo);
         ConstraintLayout constraintlayout = findViewById(R.id.login_page_constraintlayout);
 
-        logoImageView.setOnClickListener(v -> Utils.hideKeyboard(context, v));
-        constraintlayout.setOnClickListener(v -> Utils.hideKeyboard(context, v));
+        logoImageView.setOnClickListener(v -> hideKeyboard(context, v));
+        constraintlayout.setOnClickListener(v -> hideKeyboard(context, v));
 
-        ParseObject firstObject = new ParseObject("FirstClass");
-        firstObject.put("message", "Hey ! First message from android. Parse is now connected");
-        firstObject.saveInBackground(e -> {
-            if (e != null) {
-                Log.e("MainActivity", e.getLocalizedMessage());
-            } else {
-                Log.d("MainActivity", "Object saved.");
+    }
+
+    public void homepage_login(View view) {
+
+        EditText edt_login = findViewById(R.id.edt_logpage_username);
+        EditText edt_password = findViewById(R.id.edt_logpage_password);
+
+        String username = edt_login.getText().toString();
+        if (!isValidUsername(username,context)) return;
+
+        String password = edt_password.getText().toString();
+        if (!isValidPassword(password, context)) return;
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Login successful
+                    // You can perform actions such as starting a new activity or displaying a toast message here
+                    // Assume the user has successfully logged in
+                    saveLoginState(context, true);
+                    // Start the HomeActivity
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    // Finish the LoginActivity so that the user cannot navigate back to it using the back button
+                    finish();
+
+                } else {
+                    // Login failed
+                    Toast.makeText(context, "Login failed. Please check your username and password and try again.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
-
-    public void homepage_login(View view) {
-
-    }
-
     public void homepage_signup(View view) {
-
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
+
 
 }
--- app/src/main/java/fr/ouimaz/cpoapp/Utils.java
+++ app/src/main/java/fr/ouimaz/cpoapp/Utils.java
@@ -1,10 +1,16 @@
 package fr.ouimaz.cpoapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 public class Utils {
+
+    private static String invalidFieldMessageError = "";
+    private static String invalidFieldMessageError2 = "";
+    private static String invalidFieldMessageError3 = "";
 
     /**
      * Utility method to hide the soft keyboard.
@@ -15,5 +21,65 @@ public class Utils {
     public static void hideKeyboard(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * Checks if the given username is valid.
+     *
+     * @param username The username to be validated.
+     * @return true if the username is valid, false otherwise.
+     */
+    public static boolean isValidUsername(String username, Context context) {
+        // Check if username is not null and has at least 6 characters
+        if (username != null && username.length() >= 6) {
+            // Check if username contains only letters and numbers
+            return username.matches("[a-zA-Z0-9]+");
+        } else {
+            Toast.makeText(context, "Invalid username format. Usernames must be at least 6 characters long and can only contain letters and numbers.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the given password is valid based on the following rules:
+     * - The password must not be null and must have at least 8 characters.
+     * - The password must contain at least one uppercase letter, one lowercase letter, and one digit.
+     * If the password is not valid, it shows a Toast message to the user with a warning.
+     *
+     * @param password The password to be validated.
+     * @return true if the password is valid, false otherwise.
+     */
+    public static boolean isValidPassword(String password, Context context) {
+        // Check if password is not null and has at least 8 characters
+        if (password != null && password.length() >= 8) {
+            // Check if password contains at least one uppercase letter, one lowercase letter, and one digit
+            if (password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")) {
+                return true;
+            } else {
+                Toast.makeText(context, "Password must include at least one uppercase letter, one lowercase letter, and one digit.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(context, "Password must have at least 8 characters.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    /**
+     * Shows a message to the user that the username format they have entered is not valid.
+     *
+     * @param context the context in which the message should be shown
+     */
+    public static void showInvalidUsernameMessage(Context context) {
+        Toast.makeText(context, invalidFieldMessageError, Toast.LENGTH_LONG).show();
+        Toast.makeText(context, invalidFieldMessageError2, Toast.LENGTH_LONG).show();
+        Toast.makeText(context, invalidFieldMessageError3, Toast.LENGTH_LONG).show();
+    }
+
+    public static void saveLoginState(Context context, boolean isLoggedIn) {
+        SharedPreferences sharedPref = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean("isLoggedIn", isLoggedIn);
+        editor.apply();
     }
 }
